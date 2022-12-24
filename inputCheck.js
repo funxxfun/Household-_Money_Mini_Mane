aa33a99c8411e5afe34dd69232f2ae31cb9dfae9
Fumi <23shirasawa@gmail.com>
2022-12-24 22:26:02 +0900

    [Create]radiobutton-on-off

--- inputCheck.js
+++ inputCheck.js
@@ -1,9 +1,9 @@
 // ラジオボタンのカテゴリを有効無効切り替え
 function disableSelectBox(disabled) {
-
+  document.getElementById("category").disabled = disabled;
 }
 
 // 収支入力フォームの内容チェック
 function inputCheck() {
-  
+
 }
